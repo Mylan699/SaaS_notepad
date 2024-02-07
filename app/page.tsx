62f880c22fe18b5af1bb8fd432e88045bf3a220e
Mylan699 <mylan.mahieddine@ecole-isitech.fr>
2024-02-07 11:39:16 +0100

    redirection utilisateur log ou pas

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 
-export default function Home() {
+
+export default async function Home() {
+  const { isAuthenticated } = getKindeServerSession();
+
+  if(await isAuthenticated()) {
+    return redirect("/dashboard");
+  }
   return (
     <section className="flex items-center justify-center bg-background h-[90vh]">
       <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
@@ -16,9 +24,8 @@ export default function Home() {
             <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">Créer une note facilement</h1>
 
             <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl textsecondary-foreground">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis imperdiet eros.
-              Vivamus auctor lorem vel enim cursus, sed ornare tortor dictum. Donec in facilisis lacus,
-              quis ultricies turpis. Nam at tempus risus. Nulla a ex congue, ullamcorper turpis quis, egestas magna.
+              
+Découvrez notre solution SaaS innovante de prise et gestion de notes, conçue pour dynamiser votre productivité et organiser vos idées avec une facilité déconcertante. Notre plateforme web intuitive vous permet de capturer, de gérer et de partager vos notes et vos idées depuis n&apos;importe quel appareil, assurant une synchronisation parfaite entre vos différents supports. Que vous soyez en réunion, en conférence ou simplement en train de brainstormer seul, notre outil s&apos;adapte à vos besoins.
             </p>
           </div>
 

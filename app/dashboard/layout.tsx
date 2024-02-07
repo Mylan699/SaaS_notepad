83e995228243557421f40f14297ac637074b7c87
Mylan699 <mylan.mahieddine@ecole-isitech.fr>
2024-02-07 15:40:27 +0100

    prisma User ok

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,6 +2,42 @@ import { ReactNode } from "react";
 import { DashboardNav } from "../components/DashbordNav";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import prisma from "../lib/db";
+
+async function getData({
+    email,
+    id,
+    firstName,
+    lastName,
+    profileImage,
+}: {
+    email: string;
+    id: string;
+    firstName: string | undefined | null;
+    lastName: string | undefined | null;
+    profileImage: string | undefined | null;
+}) {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: id,
+        },
+        select: {
+            id: true,
+            stripeCustomerId: true,
+        },
+    });
+
+    if (!user) {
+        const name = `${firstName ?? ""} ${lastName ?? ""}`;
+        await prisma.user.create({
+            data: {
+                id: id,
+                email: email,
+                name: name,
+            },
+        });
+    }
+}
 
 
 export default async function DashboardLayout({ children }: { children: ReactNode }) {
@@ -11,6 +47,13 @@ export default async function DashboardLayout({ children }: { children: ReactNod
     if (!user) {
         return redirect('/')
     }
+    await getData({
+        email: user.email as string,
+        firstName: user.given_name as string,
+        id: user.id as string,
+        lastName: user.family_name as string,
+        profileImage: user.picture,
+    });
 
     return (
         <div className="flex flex-col space-y-6 mt-10">

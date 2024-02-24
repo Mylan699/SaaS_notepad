334a2101a8489bdf7e7bdbd35330e6d8e1ac105e
Mylan699 <mylan.mahieddine@ecole-isitech.fr>
2024-02-24 20:08:08 +0100

    Utilisateur session check

--- app/api/auth/[kindeAuth]/webhook/stripe/route.ts
+++ app/api/auth/[kindeAuth]/webhook/stripe/route.ts
@@ -1,17 +1,30 @@
 import { headers } from "next/headers"
+import { stripe } from "@/app/lib/stripe";
 import Stripe from "stripe"
 
 
 export async function POST(req: Request) {
     const body = await req.text()
 
-    const signature = headers().get('Stripe-signature')
+    const signature = headers().get('Stripe-signature') as string;
 
     let event: Stripe.Event
 
     try {
-
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET as string
+        );
     } catch (error: unknown) {
         return new Response('webhook error', { status: 404 });
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    if(event.type === 'checkout.session.completed') {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        );
     }
 }
